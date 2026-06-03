3711b700b16226d6ea9a7d3ffe637bc5b26bf05f
AliAsjad512 <aa0504503@gmail.com>
2026-06-03 22:23:35 +0000

    ci project

---
+++ tests/index.test.js
@@ -0,0 +1,21 @@
+// __tests__/index.test.js
+
+const { add, subtract, multiply } = require('../src/index');
+
+describe('Math functions', () => {
+  test('add: 2 + 3 should equal 5', () => {
+    expect(add(2, 3)).toBe(5);
+  });
+
+  test('add: negative numbers', () => {
+    expect(add(-1, -2)).toBe(-3);
+  });
+
+  test('subtract: 10 - 4 should equal 6', () => {
+    expect(subtract(10, 4)).toBe(6);
+  });
+
+  test('multiply: 3 * 4 should equal 12', () => {
+    expect(multiply(3, 4)).toBe(12);
+  });
+});
